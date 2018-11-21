d622a2326b95819886e3fd6000d24a8ba4cd8cef
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-21 16:23:47 +0000

    first ssection complete

---
+++ start_point/my_functions.rb
@@ -0,0 +1,12 @@
+def add_array_lengths(citrus, berries)
+#count the number of elements in each array
+count1 = citrus.count
+count2 = berries.count
+
+#add the number of elelments together to give a result
+total_count = count1 + count2
+#convert the results into integer
+return total_count.to_i
+#return the value
+
+end
