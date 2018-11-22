7d67f3bf9150163bae9f42378dba7a0fe4bae8b0
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-22 09:28:51 +0000

    Homework day 3 Shabana

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -2,11 +2,17 @@ def add_array_lengths(citrus, berries)
 #count the number of elements in each array
 count1 = citrus.count
 count2 = berries.count
-
 #add the number of elelments together to give a result
 total_count = count1 + count2
 #convert the results into integer
 return total_count.to_i
 #return the value
+end
+
+def sum_array(numbers)
+  #reads the number of elements in array.
+  #add the numbers together
+result = numbers.each{|a| sum+=a}
+return result
 
 end
